e74eb5d2e327c13250b61ecf69f1ddcfbea4e4fd
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2020-10-17 19:30:46 +0300

    change password

--- crm/settings.py
+++ crm/settings.py
@@ -129,11 +129,10 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 
-#SMTP Configuration
-
+# SMTP Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'henryfrstr@gmail.com'
-EMAIL_HOST_PASSWORD = 'Hsa30871246'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
